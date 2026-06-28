d3a9b811d3c0db895d1d76cd5c1ae23155389caa
UFierro13212 <uriel.fierro@udla.edu.ec>
2026-06-28 12:59:13 -0500

    Ingreso, Cálculo y Procesamiento

--- aire.c
+++ aire.c
@@ -1,4 +1,58 @@
-void limpiarBuffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
+void ingresarDatos(Zona zonas[]) {
+    int i;
+
+    for (i = 0; i < ZONAS; i++) {
+        printf("\n--- Zona %d ---\n", i + 1);
+
+        printf("Nombre de la zona: ");
+        fgets(zonas[i].nombre, 30, stdin);
+        zonas[i].nombre[strcspn(zonas[i].nombre, "\n")] = '\0';
+
+        printf("PM2.5: ");
+        scanf("%f", &zonas[i].pm25);
+
+        printf("CO2: ");
+        scanf("%f", &zonas[i].co2);
+
+        printf("Temperatura: ");
+        scanf("%f", &zonas[i].temperatura);
+
+        printf("Humedad: ");
+        scanf("%f", &zonas[i].humedad);
+
+        printf("Velocidad del viento: ");
+        scanf("%f", &zonas[i].viento);
+
+        limpiarBuffer();
+    }
+}
+
+void calcularPrediccion(Zona zonas[]) {
+    int i;
+
+    for (i = 0; i < ZONAS; i++) {
+        zonas[i].prediccion =
+            (zonas[i].pm25 * 0.45) +
+            (zonas[i].co2 * 0.25) +
+            (zonas[i].humedad * 0.10) +
+            (zonas[i].temperatura * 0.10) -
+            (zonas[i].viento * 0.10);
+    }
+}
+
+void generarAlertas(Zona zonas[]) {
+    int i;
+
+    for (i = 0; i < ZONAS; i++) {
+        if (zonas[i].prediccion < 50) {
+            strcpy(zonas[i].alerta, "BAJA");
+            strcpy(zonas[i].medida, "Calidad del aire aceptable.");
+        } else if (zonas[i].prediccion < 100) {
+            strcpy(zonas[i].alerta, "MEDIA");
+            strcpy(zonas[i].medida, "Evitar actividad fisica intensa al aire libre.");
+        } else {
+            strcpy(zonas[i].alerta, "ALTA");
+            strcpy(zonas[i].medida, "Reducir trafico y evitar exposicion prolongada.");
+        }
+    }
 }
