a12b096e8961f62d48b6744f003a85c7b826c323
UFierro13212 <uriel.fierro@udla.edu.ec>
2026-06-28 12:53:55 -0500

    ESTRUCTURAS

--- aire.c
+++ aire.c
@@ -1,4 +1,11 @@
-#include <stdio.h>
-#include <string.h>
-
-#define ZONAS 5
+typedef struct {
+    char nombre[30];
+    float pm25;
+    float co2;
+    float temperatura;
+    float humedad;
+    float viento;
+    float prediccion;
+    char alerta[30];
+    char medida[100];
+} Zona;
