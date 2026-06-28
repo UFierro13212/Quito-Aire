172da9f9ce5099371efe90c18483130abb6137b6
UFierro13212 <uriel.fierro@udla.edu.ec>
2026-06-28 12:55:55 -0500

    FUNCIONES AUXILIARES

--- aire.c
+++ aire.c
@@ -1,11 +1,4 @@
-typedef struct {
-    char nombre[30];
-    float pm25;
-    float co2;
-    float temperatura;
-    float humedad;
-    float viento;
-    float prediccion;
-    char alerta[30];
-    char medida[100];
-} Zona;
+void limpiarBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
